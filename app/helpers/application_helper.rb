432f6a9a9ec222cf98eca10d9b90ed0c3c82729e
cloudliner <cloudliner.jp@gmail.com>
2016-11-17 20:58:27 +0900

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutrial Sample App"
+    if page_title.empity?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
