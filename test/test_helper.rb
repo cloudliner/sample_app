ee39ada70c6c6b5b9d55d0f3220b9b1cb5926e41
cloudliner <cloudliner.jp@gmail.com>
2016-11-23 01:33:47 +0900

    add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,17 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
+require 'simplecov'
 require 'coveralls'
 
 Minitest::Reporters.use!
 Coveralls.wear!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
