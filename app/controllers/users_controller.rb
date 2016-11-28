6192cbb27ba570cec9efd5fa1f25bdf42906df08
cloudliner <cloudliner.jp@gmail.com>
2016-11-28 19:46:33 +0900

    add test for delete user and fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destory
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destory
+    User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
